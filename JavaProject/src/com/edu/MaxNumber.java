f274db369e917c1bc7a6a843df66fe5e3233b59a
BANDIMAHES <138276861+BANDIMAHES@users.noreply.github.com>
2023-11-02 15:56:49 +0530

    Add files via upload

---
+++ JavaProject/src/com/edu/MaxNumber.java
@@ -0,0 +1,16 @@
+package com.edu;
+
+public class MaxNumber {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int  max;
+		
+		max = (a > b) ? a : b;
+		System.out.println("the max number is "+ max);
+		
+		
+	}
+
+}
