cec17c0d3d3dba9f8a581a32fbb8524b055ad63b
BANDIMAHES <138276861+BANDIMAHES@users.noreply.github.com>
2023-11-02 16:02:30 +0530

    Add files via upload

---
+++ Factorialex.java
@@ -0,0 +1,15 @@
+
+public class Factorialex {
+
+	public static void main(String[] args) {
+		int i,fact = 1;
+		int  number = 5;
+		for(i=1;i<=number;i++) {
+			fact=fact*i;
+			
+		}
+		System.out.println(fact);
+
+	}
+
+}
